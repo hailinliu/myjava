8ea953c06d121915c3b65c257a4bb7dce4a0192c
microhan <295164745@qq.com>
2016-05-06 14:07:45 +0800

    a

--- view/user.py
+++ view/user.py
@@ -89,6 +89,7 @@ class UserHomeHandler(BaseHandler):
 
     @BaseHandler.authenticated
     def get(self):
+        # type_list = ["in", "pet_produce", "qianggou"]
         type_list = ["pet_produce"]
         total_consume = 0
         records_result = self.db.jinbi.aggregate(
