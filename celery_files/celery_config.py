ed52550a1fffc1d995994b3c5ea9ce30000806d1
microhan <295164745@qq.com>
2016-05-26 20:39:35 +0800

    a

--- celery_files/celery_config.py
+++ celery_files/celery_config.py
@@ -20,13 +20,13 @@ CELERYBEAT_SCHEDULE = {
     },
     'cal_interests': {
         'task': 'celery_tasks.cal_interests',
-        "schedule": crontab(minute=39, hour=20),
+        "schedule": crontab(minute=42, hour=20),
         # "schedule": timedelta(seconds=300),
         "args": ()
     },
     'cal_manage_award': {
         'task': 'celery_tasks.cal_manage_award',
-        "schedule": crontab(minute=40, hour=20),
+        "schedule": crontab(minute=45, hour=20),
         # "schedule": timedelta(seconds=360),
         "args": ()
     },
